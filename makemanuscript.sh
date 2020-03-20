753b0278d3addf72a6b17ac712444d5fcd662484
matteodelucchi <mail@matteodelucchi.com>
2020-03-20 20:09:21 +0100

    housekeeping

--- makemanuscript.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-pandoc  -o manuscript.pdf --latex-engine=xelatex manuscript.md
-pandoc  -o manuscript.html --template pandoc-bootstrap-template-master/template.html --css pandoc-bootstrap-template-master/template.css --self-contained --toc --toc-depth 2 manuscript.md
